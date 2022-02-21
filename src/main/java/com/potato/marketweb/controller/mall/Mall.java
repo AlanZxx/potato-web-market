58c5648abcfe9905a76ae12b968905fc412599a2
unknown <729422899@qq.com>
2022-02-21 18:21:43 +0800

    sec

--- src/main/java/com/potato/marketweb/controller/mall/Mall.java
+++ src/main/java/com/potato/marketweb/controller/mall/Mall.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -23,6 +25,7 @@ public class Mall {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    //    查询所有列表
     @ResponseBody
     @RequestMapping("/getMallTypeList")
     public Result getMallTypeList() {
@@ -33,17 +36,45 @@ public class Mall {
         return Result.ok(mallTypes);
     }
 
+    //    新增种类
     @ResponseBody
     @RequestMapping(value = "/addMallType", method = RequestMethod.POST)
     @Validated
     public Result addMallType(@RequestParam(value = "name") String name, @RequestParam(value = "detail", required = false) String detail) {
         System.out.println(name);
         System.out.println(detail);
-//        List<MallType>  mallTypes = jdbcTemplate.queryForList("SELECT * from malltype where typeName = "+ name, MallType.class);
-//        if(mallTypes.size()!=0){
-//            return Result.fail("当前已有种类");
-//        }
-//        jdbcTemplate.execute("insert malltype (typeName) values("+name+")");
+        List mallTypes = jdbcTemplate.queryForList("SELECT * from malltype where typeName = " + name);
+        if (mallTypes.size() != 0) {
+            return Result.fail("当前已有种类");
+        }
+        String dateNowStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTimeInMillis());
+        System.out.println(dateNowStr);
+        String sql = "insert malltype (typeName,addTime,detail) values('" + name + "','" + dateNowStr + "','" + detail + "')";
+        System.out.println(sql);
+        jdbcTemplate.execute(sql);
         return Result.ok("添加成功");
+    }
+
+    //    删除种类
+    @ResponseBody
+    @RequestMapping(value = "/delMallType", method = RequestMethod.POST)
+    @Validated
+    public Result delMallType(@RequestParam(value = "idList", required = false) String idList) {
+        String sql = "delete from malltype where typeId in (" + idList + ")";
+        System.out.println(sql);
+        jdbcTemplate.execute(sql);
+        return Result.ok("删除成功");
+    }
+
+    //    修改种类
+    @ResponseBody
+    @RequestMapping(value = "/modMallType", method = RequestMethod.POST)
+    @Validated
+    public Result modMallType(@RequestParam(value = "requestData", required = false) String requestData) {
+        System.out.println(requestData);
+//        String sql = "delete from malltype where typeId in (" + idList + ")";
+//        System.out.println(sql);
+//        jdbcTemplate.execute(sql);
+        return Result.ok("修改成功");
     }
 }
